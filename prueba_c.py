0d1dfff558b5f29372e9e0467b2a981463ddffe0
Luis Mogro <luismogro65@gmail.com>
2022-04-20 12:44:24 -0300

    ordenar vector

---
+++ prueba_c.py
@@ -0,0 +1,14 @@
+vectpar=[14,58,2,33,44]
+aux=0
+band=0
+    
+while band==0:
+    band=1
+    for i in range(0,len(vectpar)-1):
+        if vectpar[i] > vectpar[i+1] :
+            aux= vectpar[i]
+            vectpar[i]= vectpar[i+1]
+            vectpar[i+1]=aux
+            band=0 
+
+print(vectpar)
